ad78e7e5e0cd74da8889045ca590916bdfdf0d68
TusharPDev <97661665+TusharPDev@users.noreply.github.com>
2024-02-05 09:05:21 +0000

    task01

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 //Here i've used object type annotations.
-const sumOfTwoNums = (nums:{num1: number; num2: number}) =>{
+const sumOfTwoNums = (nums: { num1: number; num2: number }) => {
   return nums.num1 + nums.num2
 }
 
-const result  = sumOfTwoNums({num1:5,num2:20});
+const result = sumOfTwoNums({ num1: 5, num2: 20 });
 console.log(`Sum of two numbers is ${result}`);
