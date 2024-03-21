915208316d1540bdcc4debfa89da5de30159ac7b
Tushar shamkant patil <97661665+TusharPDev@users.noreply.github.com>
2024-03-21 10:08:19 +0000

    .

--- calculatorUtility.ts
+++ calculatorUtility.ts
@@ -1,45 +1,46 @@
 
 export class Calculator {
-    protected _result!: number;
+  protected _result!: number;
 
-  
-    getResult(): number {
-        return this._result;
-    }
 
-  
-    calculate(a: number, b: number): void {
-        throw new Error("Method 'calculate' must be implemented.");
-    }
+  getResult(): number {
+    return this._result;
+  }
+
+
+  calculate(a: number, b: number): void {
+    throw new Error("Method 'calculate' must be implemented.");
+  }
 }
 
 
 export class AdditionCalculator extends Calculator {
-    
-    calculate(a: number, b: number): void {
-        this._result = a + b;
-    }
+
+  calculate(a: number, b: number): void {
+    this._result = a + b;
+  }
 }
 
 
 export class SubtractionCalculator extends Calculator {
-    calculate(a: number, b: number): void {
-        this._result = a - b;
-    }
+  calculate(a: number, b: number): void {
+    this._result = a - b;
+  }
 }
 
-export class MultiplicationCalculator extends Calculator {
-    // Override the calculate method for multiplication
-    calculate(a: number, b: number): void {
-        this._result = a * b;
-    }
-}
 
 export class DivisionCalculator extends Calculator {
-    calculate(a: number, b: number): void {
-        if (b === 0) {
-            throw new Error("Cannot divide by zero.");
-        }
-        this._result = a / b;
+  calculate(a: number, b: number): void {
+    if (b === 0) {
+      throw new Error("Cannot divide by zero.");
     }
+    this._result = a / b;
+  }
+}
+
+
+export class MultiplicationCalculator extends Calculator {
+  calculate(a: number, b: number): void {
+    this._result = a * b;
+  }
 }
